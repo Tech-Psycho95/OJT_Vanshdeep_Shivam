aea5af09bb51f3c368c473e4a4e6ee869509edaa
Vanshdeeo <vanshdeepm2023@gmail.com>
2025-11-26 17:11:29 +0530

    major changes for MF platform

--- script.js
+++ script.js
@@ -9,49 +9,49 @@ const aboutDropdown = document.getElementById("aboutDropdown")
 
 const clickable = document.querySelectorAll(".click-animate")
 
+const menuToggle = document.getElementById("menuToggle")
+const navLinks = document.getElementById("navLinks")
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active")
+})
+
 homeBtn.addEventListener("click", () => {
-    window.location.href = "index.html"
+  window.location.href = "index.html"
 })
 
 accountBtn.addEventListener("click", (e) => {
-    e.stopPropagation()
-    learnDropdown.style.display = "none"
-    aboutDropdown.style.display = "none"
-    accountDropdown.style.display =
-        accountDropdown.style.display === "flex" ? "none" : "flex"
+  e.stopPropagation()
+  learnDropdown.style.display = "none"
+  aboutDropdown.style.display = "none"
+  accountDropdown.style.display = accountDropdown.style.display === "flex" ? "none" : "flex"
 })
 
 learnBtn.addEventListener("click", (e) => {
-    e.stopPropagation()
-    accountDropdown.style.display = "none"
-    aboutDropdown.style.display = "none"
-    learnDropdown.style.display =
-        learnDropdown.style.display === "flex" ? "none" : "flex"
+  e.stopPropagation()
+  accountDropdown.style.display = "none"
+  aboutDropdown.style.display = "none"
+  learnDropdown.style.display = learnDropdown.style.display === "flex" ? "none" : "flex"
 })
 
 aboutBtn.addEventListener("click", (e) => {
-    e.stopPropagation()
-    accountDropdown.style.display = "none"
-    learnDropdown.style.display = "none"
-    aboutDropdown.style.display =
-        aboutDropdown.style.display === "flex" ? "none" : "flex"
+  e.stopPropagation()
+  accountDropdown.style.display = "none"
+  learnDropdown.style.display = "none"
+  aboutDropdown.style.display = aboutDropdown.style.display === "flex" ? "none" : "flex"
 })
 
-accountDropdown.addEventListener("click", (e) => e.stopPropagation())
-learnDropdown.addEventListener("click", (e) => e.stopPropagation())
-aboutDropdown.addEventListener("click", (e) => e.stopPropagation())
-
 window.addEventListener("click", () => {
-    accountDropdown.style.display = "none"
-    learnDropdown.style.display = "none"
-    aboutDropdown.style.display = "none"
+  accountDropdown.style.display = "none"
+  learnDropdown.style.display = "none"
+  aboutDropdown.style.display = "none"
 })
 
 clickable.forEach((item) => {
-    item.addEventListener("click", () => {
-        item.classList.add("pop")
-        setTimeout(() => {
-            item.classList.remove("pop")
-        }, 180)
-    })
+  item.addEventListener("click", () => {
+    item.classList.add("pop")
+    setTimeout(() => {
+      item.classList.remove("pop")
+    }, 180)
+  })
 })
